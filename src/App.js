2247c5c541c0dff32e4c51d5b891aba0df02b277
Muhammad Samran <samransam12@gmail.com>
2023-12-03 23:07:38 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
-import './App.css';
-import HeaderBar from './components/HeaderBar';
-import SiteCards from './components/SiteCards';
-import { BrowserRouter as Router, Link, Route, Routes, Redirect } from 'react-router-dom'
-import LoginForm from './users/LoginForm'
-import SignUpForm from './users/SignUpForm';
-import CurrentUserProvider from './contexts/CurrentUser';
-import { BrowserRouter } from 'react-router-dom';
-import NewRecipe from './components/NewRecipe';
+import "./App.css";
+import HeaderBar from "./components/HeaderBar";
+import SiteCards from "./components/SiteCards";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes,
+  Redirect,
+} from "react-router-dom";
+import LoginForm from "./users/LoginForm";
+import SignUpForm from "./users/SignUpForm";
+import CurrentUserProvider from "./contexts/CurrentUser";
+import { BrowserRouter } from "react-router-dom";
+import NewRecipe from "./components/NewRecipe";
 
-import Recipes from './components/Recipes';
+import Recipes from "./components/Recipes";
 
-import About from './components/About';
-
+import About from "./components/About";
+import IntroCard from "./components/IntroCard";
 
 function App() {
-
   return (
     <CurrentUserProvider>
       <BrowserRouter>
         <HeaderBar />
-        <SiteCards />
+        {/* <SiteCards /> */}
         <Routes>
+          <Route path="/" element={<IntroCard />} />
           <Route path="/sign-up" element={<SignUpForm />} />
           <Route path="/login" element={<LoginForm />} />
-          <Route path="/about" element={<About />} /> 
-          <Route path="/recipes" element={<Recipes />} /> 
+          <Route path="/about" element={<About />} />
+          <Route path="/recipes" element={<Recipes />} />
           <Route path="/newRecipe" element={<NewRecipe />} />
-        
-            
         </Routes>
       </BrowserRouter>
     </CurrentUserProvider>
