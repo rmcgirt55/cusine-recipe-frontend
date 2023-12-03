e7e29aeefd9acb743100cd2fc72063649b48388a
Muhammad Samran <samransam12@gmail.com>
2023-12-03 23:08:29 +0500

    changes

--- src/components/SiteCards.js
+++ src/components/SiteCards.js
@@ -1,49 +1,65 @@
-import { Link } from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
-import Nav from 'react-bootstrap/Nav';
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Nav from "react-bootstrap/Nav";
 import { useContext } from "react";
-import { CurrentUser } from '../contexts/CurrentUser';
+import { CurrentUser } from "../contexts/CurrentUser";
 
 function SiteCards() {
+  const { currentUser } = useContext(CurrentUser);
 
-    const { currentUser } = useContext(CurrentUser)
+  return (
+    <>
+      {!currentUser ? (
+        <Card className="navigation-main">
+          <Card.Header>
+            <Nav
+              variant="tabs"
+              defaultActiveKey="#first"
+              className="justify-content-center"
+            >
+              <Nav.Item>
+                <Nav.Link as={Link} to="/about">
+                  About
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/recipes">
+                  Recipes
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Card.Header>
+        </Card>
+      ) : (
+        <Card>
+          <Card.Header>
+            <Nav
+              variant="tabs"
+              defaultActiveKey="#first"
+              className="justify-content-center"
+            >
+              <Nav.Item>
+                <Nav.Link as={Link} to="/about">
+                  About
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/recipes">
+                  Recipes
+                </Nav.Link>
+              </Nav.Item>
 
-    return (
-        <>{
-            !currentUser ? <Card>
-                <Card.Header>
-                    <Nav variant="tabs" defaultActiveKey="#first" className="justify-content-center">
-                        <Nav.Item>
-                            <Nav.Link as={Link} to="/about">About</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link as={Link} to="/recipes">Recipes</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                </Card.Header>
-            </Card>
-                :
-                <Card>
-                    <Card.Header>
-                        <Nav variant="tabs" defaultActiveKey="#first" className="justify-content-center">
-                            <Nav.Item>
-                                <Nav.Link as={Link} to="/about">About</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link as={Link} to="/recipes">Recipes</Nav.Link>
-                            </Nav.Item>
-                            
-                            <Nav.Item>
-                                <Nav.Link as={Link} to="/newRecipe">Add Recipe</Nav.Link>
-                            </Nav.Item>
-                            
-                        </Nav>
-                    </Card.Header>
-                </Card>
-        }
-        </>
-    )
+              <Nav.Item>
+                <Nav.Link as={Link} to="/newRecipe">
+                  Add Recipe
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Card.Header>
+        </Card>
+      )}
+    </>
+  );
 }
 
 export default SiteCards;
-
